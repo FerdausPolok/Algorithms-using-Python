c9cf261bcea433ebc34e9f7b19a8b019794a3d30
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-27 08:49:03 +0600

    Update linearSearch.py

--- Searching Algo/linearSearch.py
+++ Searching Algo/linearSearch.py
@@ -1,12 +1,19 @@
 print("Linear Search using Python")
 
 list1=[]
-list1= list(input("Please Enter the list without any space: "))
-key = input("Please enter the key you want to find: ")
+list11= list(input("Please Enter the list without any space: "))
+key1 = input("Please enter the key you want to find: ")
 
-for index in range (len(list1)):
-    if list1[index] == key:
-        print(key, "Found on",index, "no. position!")
 
+def solution(list1,key):
+
+    for index in range (len(list1)):
+        if list1[index] == key:
+         print(key, "Found on",index, "no. position!")
+
+    if index == len(list1)-1 and list1[index]!= key:
+        print(key, "is absent on your given list!")
+
+solution(list11,key1)        
 
          
