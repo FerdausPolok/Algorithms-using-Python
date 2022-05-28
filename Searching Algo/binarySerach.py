14a37a5f29e7afecadf839dfb8139cb13e4f3a3c
Ferdaus Zaman Polok <ferdaus15-9924@diu.edu.bd>
2022-05-28 09:26:18 +0600

    Binary search final

--- Searching Algo/binarySerach.py
+++ Searching Algo/binarySerach.py
@@ -1,7 +1,52 @@
+from ast import Num
+from turtle import left
+
+
 print("Binary Search using Python")
 
-strr= input("Please Enter the list by separating the item using black space: ")
+strr= input("Please Enter a sorted list by separating the item using black space: ")
 list1= list(strr.split(" "))
-list1.sort(key=str.lower)
-print(list1)
+# list1.sort(key=str.lower)
+
+#print("Finding from this sorted list", list1)
+
+find = input("Enter the value you want to check: ")
+
+def testLocation(mid, list, key):
+    
+    if list[mid] == key:
+        if mid-1 >= 0 and list[mid-1] == key:
+            return 'left'
+        else:
+            return 'found'
+    elif list[mid] < key:
+        return 'left'
+    else:
+        return 'right'      
+
+def binarySearch(list, key):
+    lo, hi = 0, len(list) - 1
+    while lo <= hi:
+        mid = (lo + hi) // 2
+        result = testLocation(mid, list, key)
+        if result == 'found':
+            return mid
+        elif result == 'left':
+            hi = mid - 1
+        elif result == 'right':
+            lo = mid + 1
+    return -1
+foundOn = binarySearch(list1, find)
+if foundOn!=-1:
+    print("You given input", find, "was found on", foundOn, "no position!!")
+else:
+    print(find, "is absent on the list!")    
+
+
+
+
+
+
+
+      
     
